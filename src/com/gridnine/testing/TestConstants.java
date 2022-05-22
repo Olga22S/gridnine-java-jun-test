6ae040f5d91809f1895d8eca626a0053d960fe13
Olga22S <22031992Les>
2022-05-22 17:39:31 +0300

    Tests created commit

--- src/com/gridnine/testing/TestConstants.java
+++ src/com/gridnine/testing/TestConstants.java
@@ -11,13 +11,11 @@ public class TestConstants {
     private static final LocalDateTime ARRIVAL2 = DEPARTURE2.plusHours(3);
     private static final LocalDateTime DEPARTURE3 = ARRIVAL2.plusHours(2);
     private static final LocalDateTime ARRIVAL3 = DEPARTURE3.plusHours(1);
-
     public static final Segment SEGMENT1 = new Segment(DEPARTURE1, ARRIVAL1);
     public static final Segment SEGMENT2 = new Segment(DEPARTURE1.minusDays(2), ARRIVAL1.minusDays(2));
     public static final Segment SEGMENT3 = new Segment(DEPARTURE2, ARRIVAL2);
     public static final Segment SEGMENT4 = new Segment(DEPARTURE3, ARRIVAL3);
     public static final Segment SEGMENT5 = new Segment(ARRIVAL1, DEPARTURE1);
-
     //A normal flight
     public static final Flight FLIGHT1 = new Flight(Arrays.asList(SEGMENT1));
     //A flight departing in the past
@@ -28,6 +26,4 @@ public class TestConstants {
     public static final Flight FLIGHT4 = new Flight(Arrays.asList(SEGMENT1, SEGMENT3, SEGMENT4));
     //A flight that departs before it arrives
     public static final Flight FLIGHT5 = new Flight(Arrays.asList(SEGMENT5));
-
-
 }
